6fd7277f0451de5260fac03fb908bea524faf69b
Matheus de Souza <mws.souza@gmail.com>
2018-03-15 09:17:33 -0300

    Add smoke tests

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,35 +1,29 @@
 import { expect } from 'chai';
-import FizzBuzz from '../src/main';
+import { search, searchAlbums, searchArtists, searchTracks, searchPlaylists } from './../src/main'
 
-/*
- * Desafio FizzBuzz
- *
- * Escreva uma lib que receba um número e:
- *
- * Se o número for divisível por 3, no lugar do número escreva 'Fizz'
- * Se o número for divisível por 5, no lugar do número escreva 'Buzz'
- * Se o número for divisível por 3 e 5, no lugar do número escreva 'FizzBuzz'
- * Se não for múltiplo de nada, retorna o número
- *
- */
-describe('FizzBuzz', () => {
-  it('should return `Fizz` when multiple of 3', () => {
-    expect(FizzBuzz(3)).to.be.equal('Fizz');
-    expect(FizzBuzz(6)).to.be.equal('Fizz');
-  });
+describe('Spotify', () => {
 
-  it('should return `Buzz` when multiple of 5', () => {
-    expect(FizzBuzz(5)).to.be.equal('Buzz');
-    expect(FizzBuzz(10)).to.be.equal('Buzz');
-  });
+    describe('Smoke Tests', () => {
+      // search (genérico) + de 1 tipo
+      // searchAlbums
+      // searchArtists
+      // searchTracks
+      // searchPlaylists
 
-  it('should return `FizzBuzz` when multiple of 3 and 5', () => {
-    expect(FizzBuzz(15)).to.be.equal('FizzBuzz');
-    expect(FizzBuzz(45)).to.be.equal('FizzBuzz');
-  });
-
-  it('should return the number when non-multiple', () => {
-    expect(FizzBuzz(7)).to.be.equal(7);
-    expect(FizzBuzz(14)).to.be.equal(14);
-  });
+      it('should exist the search method', () => {
+        expect(search).to.exist;
+      });
+      it('should exist the searchAlbums method', () => {
+        expect(searchAlbums).to.exist;
+      });
+      it('should exist the searchArtists method', () => {
+        expect(searchArtists).to.exist;
+      });
+      it('should exist the searchTracks method', () => {
+        expect(searchTracks).to.exist;
+      });
+      it('should exist the searchPlaylists method', () => {
+        expect(searchPlaylists).to.exist;
+      });
+    });
 });
