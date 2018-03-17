211af2a8f7c0422a960a99c8f922ba6347e1adc7
Matheus de Souza <mws.souza@gmail.com>
2018-03-17 13:02:18 -0300

    Add contextWith test

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -45,26 +45,37 @@ describe('Spotify', () => {
   });
 
   describe('Generic Search', () => {
+    let fetchedStub;
 
-    it('should call fetch function', () => {
-      const fetchedStub = sinon.stub(global, 'fetch');
-      const artists = search();
-  
-      expect(fetchedStub).to.have.been.calledOnce;
+    beforeEach(() => {
+      fetchedStub = sinon.stub(global, 'fetch');
+    });
 
+    afterEach(() => {
       fetchedStub.restore();
     });
 
-    it('should receive the correct url to fetch', () => {
-      const fetchedStub = sinon.stub(global, 'fetch');
-      const artists = search('Incubus', 'artist');
+    it('should call fetch function', () => {
+      const artists = search();
 
-      expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=Incubus&type=artist');
-
-
-      const albums = search('Incubus', 'album');
-      expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=Incubus&type=album');
+      expect(fetchedStub).to.have.been.calledOnce();
     });
-    
+
+    it('should receive the correct url to fetch', () => {
+      context('passing on type', () => {
+        const artists = search('Incubus', 'artist');
+        expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=Incubus&type=artist');
+  
+        const albums = search('Incubus', 'album');
+        expect(fetchedStub).to.have.been
+          .calledWith('https://api.spotify.com/v1/search?q=Incubus&type=album');
+      });
+
+      context('passing more than one type', () => {
+        const artistsAndAlbums = search('Incubus', ['artist', 'album']);
+        expect(fetchedStub).to.have.been
+          .calledWith('https://api.spotify.com/v1/search?q=Incubus&type=artist,album');
+      });
+    });
   });
-});  
+});
