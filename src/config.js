e8d74d9a99fff97b269302bcd2c88f2f6cea5fad
Matheus de Souza <mws.souza@gmail.com>
2018-03-19 13:06:37 -0300

    Add spotify token

---
+++ src/config.js
@@ -0,0 +1,7 @@
+const TOKEN_API = 'BQAVq25y4ezyrkp1j9fyzXFQrHufA8phMyVtjYHyKLh5cg1rxyYL9vfeO6POqXLlP39vgRkDqVu3Z8y7bY0ymdQxkJkEOXyE8jMVD_kOsBozpnNkeIJNhMuixi7s8rRGAAOVGBLW64x-roYW2uxspg';
+
+export const HEADERS = {
+  headers: {
+    'Authorization': `Bearer ${TOKEN_API}`,
+  },
+};
