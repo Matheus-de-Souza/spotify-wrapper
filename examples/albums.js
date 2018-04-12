28ac8293a95cb51da84bd75e7b99fcd0bbc88bcd
Matheus de Souza <mws.souza@gmail.com>
2018-04-12 13:52:58 -0300

    Update npm version and fix lib name

--- examples/albums.js
+++ examples/albums.js
@@ -4,7 +4,7 @@ global.fetch = require('node-fetch');
 import SpotifyWrapper from '../src/index';
 
 const spotify = new SpotifyWrapper({
-  token: 'BQCr-chfB5Fo856IR37nFXGM-NZPhe69t3KvmsjWIQiMuIuiuXhrc0rgM1YlEB79BsvtNb_4Sdm4CJrZfUzQn5EMEuYpaXkxg5qzs331tNGKrOq3etiUe9ZVBeSMjuWr09WWwDcSXe2KNYnqOPI',
+  token: 'BQC10782r6n2btClxVao0NawszRe5jbU-0fh4Z9iisEhoB2_OJqWB24i-Q3zRamu8kQcsoGvvejf4zn0xd3ejoc2yGgMbgAuzLtvU1o66Ej41kqu2sUTwygVmybCepRfelSULpSGaxRa9BWFbcc',
 });
 
 const albums = spotify.search.albums('Incubus');
