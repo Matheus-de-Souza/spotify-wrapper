8e546fc109ae7ed66a1e7446d15003a400d3d3cf
Matheus de Souza <mws.souza@gmail.com>
2018-04-12 11:30:19 -0300

    Improve examples

--- examples/albums.js
+++ examples/albums.js
@@ -3,11 +3,10 @@ global.fetch = require('node-fetch');
 
 import SpotifyWrapper from '../src/index';
 
-let spotify = new SpotifyWrapper({
-  token: 'BQDSgSrhYivxh9E5L9QK_4e1atraVYauzAaKFkNSP-uPqNScEEG45RoO8xHxM2BS9tI6tnPLT0VokF1RrafB1_dWef2zfK1-tCcUtUi90RWx3T0rQdYi5bm0V4BREVy1gpajDLNvRKcd0jAvZOw',
+const spotify = new SpotifyWrapper({
+  token: 'BQCr-chfB5Fo856IR37nFXGM-NZPhe69t3KvmsjWIQiMuIuiuXhrc0rgM1YlEB79BsvtNb_4Sdm4CJrZfUzQn5EMEuYpaXkxg5qzs331tNGKrOq3etiUe9ZVBeSMjuWr09WWwDcSXe2KNYnqOPI',
 });
 
 const albums = spotify.search.albums('Incubus');
 
-albums.then(data => console.log(data)); //.albums.items.map(item => console.log(item.name)));
-// albums.then(data => data.albums.items.map(item => console.log(item.name)));
+albums.then(data => data.albums.items.map(item => console.log(item.name)));
